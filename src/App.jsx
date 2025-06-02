853f3ccc4847abb0036bd3ea9a3284e9f8668ebb
quantumCode <quantumcode22@gmail.com>
2025-06-02 18:29:58 +0530

    removed gsap, went vanilla css for pinning the left section

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,50 @@
 import Card from "./components/Card";
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function App() {
-  gsap.registerPlugin(useGSAP,ScrollTrigger);
-  useGSAP(()=>{
-    gsap.to("#secActive",{
-      scrollTrigger:{
-        trigger: "#active",
-        start:"top top",
-        end:"bottom bottom",
-        scrub:1,
-        pin: "#pin",
-        markers: true
-      },
-    })
-  })
 
   return (
     <>
-      <div className=" grid grid-cols-7 grid-rows-2 gap-[45px] h-screen max-w-[calc(100vw-10rem)] m-auto ">
-        <div id="pin" className="flex flex-wrap col-span-3 row-span-2 gap-[45px]">
-        <div className="pt-[90px] w-full h-[50%] border-g">
-          <h1>I'm</h1>
-          <h1>Supriyo Purkait</h1>
-          <h2>Just a WebDev with passion to build something useful...</h2>
+      <div className="grid grid-cols-7 gap-[45px] h-fit max-w-[calc(100vw-10rem)] m-auto ">
+        <div
+          id="pin"
+          className="flex flex-wrap justify-start col-span-3 gap-[45px] h-screen sticky top-0 border-r"
+        >
+          <div className="pt-[90px] w-full border-g">
+            <h1>I'm</h1>
+            <h1>Supriyo Purkait</h1>
+            <h2>Just a WebDev with passion to build something useful...</h2>
+          </div>
+          <div className="w-full border-g">
+            <div id="secTitle1" className="">About me</div>
+            <div id="secTitle2">Projects</div>
+            <div>Experience</div>
+          </div>
         </div>
         <div
-          className="w-full h-full border-g"
+          id="active"
+          className="gap-1 col-start-4 col-span-4 flex flex-col h-fit border-g"
         >
-          list goes here...
-        </div>
-        </div>
-        <div id="active" className="gap-1 row-span-2 col-start-4 col-span-4 flex flex-col h-fit border-g">
-          <div className="flex flex-col gap-[23px] pt-[90px] border-b">
+          <div id="sec1" className="flex flex-col gap-[23px] pt-[90px] border-b">
             <Card />
             <Card />
             <Card />
             <Card />
             <Card />
           </div>
-          <div className="flex flex-col gap-[23px]">
+          <div id="sec2" className="flex flex-col gap-[23px]">
             <Card />
             <Card />
             <Card />
             <Card />
             <Card />
+        </div>
+          <div id="sec3" className="flex flex-col gap-[23px]">
             <Card />
             <Card />
             <Card />
             <Card />
             <Card />
-          </div>
+        </div>
         </div>
       </div>
     </>
